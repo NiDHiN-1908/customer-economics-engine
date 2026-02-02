aff99ddc9bd15e0733b75a1c007ad3a6d671c4d7
Nidhin S <nidhinshibu01@gmail.com>
2026-02-02 20:17:18 +0530

    v8: make CLV profit-aware by subtracting retention cost

--- src/clv_engine.py
+++ src/clv_engine.py
@@ -1,23 +1,25 @@
 from src.survival import survival_curve
+from src.cost import get_retention_cost
 
 def compute_clv(monthly_revenue, churn_prob, months=12, discount_rate=0.10):
     """
-    Full economic CLV with survival + discounting.
+    Profit-aware discounted CLV with survival.
 
-    CLV = sum_t ( revenue * survival_t * discount_t )
-
-    survival_t comes from survival curve.
-    discount_t = 1 / (1 + r/12)^t
+    CLV = sum_t ( (revenue - retention_cost) 
+                  * survival_t 
+                  * discount_t )
     """
 
     survival_probs = survival_curve(churn_prob, months)
     monthly_discount = discount_rate / 12
+    retention_cost = get_retention_cost()
 
     clv = 0
 
     for t in range(1, months + 1):
         survival_t = survival_probs[t - 1]
         discount_t = 1 / ((1 + monthly_discount) ** t)
-        clv += monthly_revenue * survival_t * discount_t
+        monthly_profit = monthly_revenue - retention_cost
+        clv += monthly_profit * survival_t * discount_t
 
     return clv
